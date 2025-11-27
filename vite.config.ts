f9d291d220f544071d0e88840c601469f6c036ee
jasoncorrea <81769974+jasoncorrea@users.noreply.github.com>
2025-11-26 23:57:52 -0300

    b 5

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // Base path set to root for Vercel deployment stability
+  base: '/', 
   build: {
     outDir: 'dist',
   }
