21cad2f023085a23ba64d50b94ead96742c95624
jasoncorrea <81769974+jasoncorrea@users.noreply.github.com>
2025-11-27 00:12:33 -0300

    tentativa 10

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Garante que caminhos relativos funcionem
+  base: '/', // Padrão recomendado para deploy na raiz (Vercel)
 })
